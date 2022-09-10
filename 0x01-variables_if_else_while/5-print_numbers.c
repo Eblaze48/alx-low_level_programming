67ab5cd8e344db46bb7d2bea47b4f51c586b5caf
Eblaze48 <emobosajike@gmail.com>
2022-09-10 07:08:51 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 100; d++)
+	{
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
+		if (d != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		
+	}
+	putchar('\n');
+
+	return (0);
+}
