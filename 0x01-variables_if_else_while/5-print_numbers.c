ceec7bcaab503ebd242200183c8a99c7ef918ff9
Eblaze48 <emobosajike@gmail.com>
2022-09-10 07:15:03 -0700

    Task

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -12,7 +12,7 @@ int main(void)
 {
 	int d;
 
-	for (d = 0; d < 100; d++)
+	for (d = 0; d < 10; d++)
 	{
 		putchar((d / 10) + '0');
 		putchar((d % 10) + '0');
@@ -21,7 +21,6 @@ int main(void)
 			putchar(',');
 			putchar(' ');
 		}
-		
 	}
 	putchar('\n');
 
