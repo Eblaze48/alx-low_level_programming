0d0e5e538f2d962d0232a26a81ec1c375ae6dd0f
Eblaze48 <emobosajike@gmail.com>
2022-09-10 15:41:31 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int d;
+	char low;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(low);
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
