0137a645cf6efc920dcd6e1e72041b455d16c404
Eblaze48 <emobosajike@gmail.com>
2022-09-10 15:23:57 -0700

    T

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int k;
+
+	for (k = '0'; k <= '9'; k++)
+		putchar(k);
+	putchar('\n');
+
+	return (0);
+}
